3a0f84f0ffb9292dadf70342a6863947e2c581e0
adyluna <adylunaleite@gmail.com>
2022-11-23 18:29:12 -0300

    Req 9 ok

---
+++ src/Monster.ts
@@ -0,0 +1,35 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number) {
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): number {
+    return enemy.receiveDamage(this._strength);
+  }
+}
